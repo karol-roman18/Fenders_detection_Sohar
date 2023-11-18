22c4dfa4def51879862317321e978db29b5d1620
karol-roman18 <karol.roman5@gmail.com>
2023-11-18 21:26:44 +0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+#I am using this because I am using the same paths in multiple scripts. Maybe this will be useful in the future as well
+video_paths = {
+    'video1': "Localyolo/TestVideos/Blanco/Side2.mp4"
+    #'video2': 'path/to/video2.mp4',
+    # Add more video paths as needed
+}
+
+print(video_paths)
